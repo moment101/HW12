3d105e91451e2fa24ff186de365cf547a0369fa5
jon <u8812012moment@gmail.com>
2023-06-04 21:34:42 +0800

    Q1 completed

--- script/Q1.s.sol
+++ script/Q1.s.sol
@@ -2,16 +2,72 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "compound-protocol/contracts/CErc20.sol";
-import "compound-protocol/contracts/CErc20Delegator.sol";
+import {MyErc20} from "../src/MyErc20.sol";
+import {CErc20} from "compound-protocol/contracts/CErc20.sol";
+import {CErc20Delegator} from "compound-protocol/contracts/CErc20Delegator.sol";
+import {WhitePaperInterestRateModel} from "compound-protocol/contracts/WhitePaperInterestRateModel.sol";
+import {CErc20Delegate} from "compound-protocol/contracts/CErc20Delegate.sol";
+
+import {Comptroller} from "compound-protocol/contracts/Comptroller.sol";
+import {Unitroller} from "compound-protocol/contracts/Unitroller.sol";
+import {SimplePriceOracle} from "compound-protocol/contracts/SimplePriceOracle.sol";
+import {CToken} from "compound-protocol/contracts/CToken.sol";
 
 contract Q1Script is Script {
-    function setUp() public {}
+    address public alice;
+    address public bob;
+
+    MyErc20 qqq;
+    Comptroller comptroller;
+    SimplePriceOracle priceOracle;
+    Unitroller unitroller;
+    WhitePaperInterestRateModel interestRateModel;
+    CErc20Delegate cErc20Delegate;
+    CErc20Delegator cErc20Delegator;
+
+    function setUp() public virtual {
+        alice = makeAddr("Alice");
+        bob = makeAddr("Bob");
+
+        vm.label(address(alice), "Alice");
+    }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.envAddress("TEST_ACCOUNT_ADDRESS");
         vm.startBroadcast(deployerPrivateKey);
 
-        vm.broadcast();
+        qqq = new MyErc20("TripleQ", "QQQ", 18);
+
+        comptroller = new Comptroller();
+        priceOracle = new SimplePriceOracle();
+        comptroller._setPriceOracle(priceOracle);
+
+        unitroller = new Unitroller();
+        unitroller._setPendingImplementation(address(comptroller));
+        unitroller._acceptImplementation();
+
+        interestRateModel = new WhitePaperInterestRateModel(0, 0);
+        cErc20Delegate = new CErc20Delegate();
+
+        cErc20Delegator = new CErc20Delegator(
+            address(qqq),
+            comptroller,
+            interestRateModel,
+            1e18, // uint initialExchangeRateMantissa_,  1:1   1e18
+            "TripleQPool",
+            "cQQQ",
+            18,
+            payable(deployerAddress),
+            address(cErc20Delegate),
+            "" // bytes memory becomeImplementationData
+        );
+
+        address[] memory cTokens = new address[](1);
+        cTokens[0] = address(cErc20Delegate);
+        comptroller.enterMarkets(cTokens);
+        comptroller._supportMarket(cErc20Delegate);
+
+        vm.stopBroadcast();
     }
 }
